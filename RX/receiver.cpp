59a1defebc00246f8dd34befc84953e288a89f77
Antonio Astorino <antonio.astorino.phd@gmail.com>
2020-06-03 00:17:22 +0200

    Implemented circular buffer

--- RX/receiver.cpp
+++ RX/receiver.cpp
@@ -6,24 +6,34 @@
 
 int main() {
     // ftok to generate unique key
-    key_t key = ftok("shmfile", 65);
+    key_t key                 = ftok("shmfile", 65);
+    const uint16_t bufferSize = 256;
 
     // shmget returns an identifier in shmid
-    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+    int shmid = shmget(key, bufferSize, 0666 | IPC_CREAT);
 
     // shmat to attach to shared memory
-    char* str = (char*)shmat(shmid, (void*)0, 0);
+        uint8_t* buffer = (uint8_t*)shmat(shmid, (void*)0, 0);
+    uint8_t* r      = &buffer[bufferSize - 2];
+    uint8_t* w      = &buffer[bufferSize - 1];
+    *r              = 0;
+    *w              = 0;
+   
+    while (true) { // keep reading until you hit the read location
+        while (*w == *r) {
+            usleep(100);
+        }
+        if (buffer[*r] == '\\') break;
+        std::cout << buffer[*r];
+        std::cout.flush();
 
-    bool done = false;
-    while (!done) {
-        if (str[0] != '\0') std::cout << "Data read from memory: " << str << "\n";
-        if (str[0] == '\\') done = true;
-        str[0] = '\0';
-        sleep(1);
+        *r = (*r + 1) % (bufferSize - 2);
+        // std::cout << "I'm done reading at location " << (int)*r << "\n";
+        // sleep(1);
     }
 
     // detach from shared memory
-    shmdt(str);
+    shmdt(buffer);
 
     // destroy the shared memory
     shmctl(shmid, IPC_RMID, NULL);
--- TX/transmitter.cpp
+++ TX/transmitter.cpp
@@ -1,34 +1,51 @@
 #include <iostream>
 #include <stdio.h>
+#include <string>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <string>
+#include <unistd.h>
+
 int main() {
     // ftok to generate unique key
-    key_t key = ftok("shmfile", 65);
+    key_t key                 = ftok("shmfile", 65);
+    const uint16_t bufferSize = 256;
 
     // shmget returns an identifier in shmid
-    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+    int shmid = shmget(key, bufferSize, 0666 | IPC_CREAT);
 
     // shmat to attach to shared memory
-    char* str = (char*)shmat(shmid, (void*)0, 0);
+    uint8_t* buffer = (uint8_t*)shmat(shmid, (void*)0, 0);
+    uint8_t* r      = &buffer[bufferSize - 2];
+    uint8_t* w      = &buffer[bufferSize - 1];
+    *r              = 0;
+    *w              = 0;
 
-    const char *cstr;
+    std::string inputStr;
 
-    std::string str1;
-
-    while (str1[0] != '\\') {
+    while (inputStr[0] != '\\') {
+        // get input line by line
         std::cout << "Write Data : ";
-        std::getline(std::cin, str1);
-        cstr = str1.c_str();
-        std::memcpy(str, cstr, str1.size());
-        str[str1.size()] = '\0'; 
+        std::getline(std::cin, inputStr);
+        uint16_t charLocation = 0;
+
+        const char* cstr = inputStr.c_str();
+
+        while (inputStr[charLocation] != 0) { // scan the string
+            while (*w == ((*r) - 1) || *w == ((*r) + 253)) {
+                usleep(100);
+            }; // wait if this is where the reader is located
+            buffer[*w] = inputStr[charLocation];
+            *w         = (*w + 1) % (bufferSize - 2);
+            charLocation++;
+        }
+
+        std::cout << "I'm done writing at location " << (int)*w << "\n";
     }
 
-    printf("Termination character received: %s\n", str);
+    printf("Termination character received\n");
 
     // detach from shared memory
-    shmdt(str);
+    shmdt(buffer);
 
     return 0;
 }
